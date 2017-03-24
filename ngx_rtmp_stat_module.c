2e539c042052e32db41358bfb7fd094ee601b09e
Nikita Yaschenko <n.yaschenko@corp.vk.com>
2017-03-24 21:45:32 +0300

    Comment unused content phase handler

--- ngx_rtmp_stat_module.c
+++ ngx_rtmp_stat_module.c
@@ -1303,9 +1303,9 @@ static ngx_int_t
 ngx_rtmp_stat_postconfiguration(ngx_conf_t *cf)
 {
     //ngx_http_handler_pt       *h;
-    ngx_http_core_main_conf_t *cmcf;
+//    ngx_http_core_main_conf_t *cmcf;
 
-    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
+//    cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 //    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
 //    if (h == NULL) {
 //        return NGX_ERROR;
